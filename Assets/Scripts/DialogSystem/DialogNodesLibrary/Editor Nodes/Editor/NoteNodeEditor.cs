752f9d4bf0b8775caaae873bb7520ff995e23dee
NoxNorthwind <nox.northwind@gmail.com>
2017-11-13 23:15:34 +0100

    NoteNode for simple editor nodes

---
+++ Assets/Scripts/DialogSystem/DialogNodesLibrary/Editor Nodes/Editor/NoteNodeEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using SpyOnHuman.DialogSystem.NodeFramework;
+
+namespace SpyOnHuman.DialogSystem
+{
+    [CustomEditor(typeof(NoteNode))]
+    public class NoteNodeEditor : NodeInspector
+    {
+        NoteNode node;
+
+        private void OnEnable()
+        {
+            node = (NoteNode)target;
+        }
+
+        public override void OnDrawNodeGUI(Rect rect)
+        {
+            node.note = EditorGUI.TextArea(rect, node.note);
+        }
+    }
+}
---
+++ Assets/Scripts/DialogSystem/DialogNodesLibrary/Editor Nodes/NoteNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SpyOnHuman.DialogSystem.NodeFramework;
+
+namespace SpyOnHuman.DialogSystem
+{
+    [NodeData("Note", "A simple node for notes.", 128f, 64f, true, 0.4f, 0.4f, 0.4f)]
+    public class NoteNode : Node
+    {
+
+        public string note;
+
+    }
+}
